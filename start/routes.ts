9103962eeff3df76eb9802e4c14f1fca0a54972d
Joaoof <joaodeus400@gmail.com>
2023-05-20 13:30:28 -0300

    erro resolvido

--- start/routes.ts
+++ start/routes.ts
@@ -2,4 +2,4 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', 'PostsController.index')
 
-Route.resource('/posts', 'PostsController.index').apiOnly()
+Route.resource('/posts', 'PostsController').apiOnly()
