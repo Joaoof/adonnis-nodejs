be1c0d6b750bdac619930acc7a5644657eb59658
Joaoof <joaodeus400@gmail.com>
2023-06-03 23:19:30 -0300

    final

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,5 +1,4 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-
 export default class AuthController {
   public async store({ request, auth }: HttpContextContract) {
     const { email, password } = request.all()
